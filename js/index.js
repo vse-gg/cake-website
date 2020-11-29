729f96fb24750c442b79f4ee4f57dbd56432a1b7
vse-gg <alinabogoslov1@gmail.com>
2020-11-29 19:40:30 +0300

    adapting

---
+++ js/index.js
@@ -0,0 +1,29 @@
+
+console.log("Denis");
+
+
+let nl;
+const str = "Alina";
+const n = 6;
+const bool = true; //fals
+const obj = {
+	name: "Alina",
+	lastName: "Sorokina",
+	age: 24,
+	married: true,
+};
+const arr = [5,5,8,3,4,1,7,2];
+
+console.log(n);
+console.log(bool,obj,arr);
+
+
+let button = document.getElementById('func_button');
+console.log([button])
+
+button.addEventListener('click', () => {
+	alert('Ты кликнул на меня!!!')
+})
+
+
+
